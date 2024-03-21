f88713340eaf8054ac0ee2ce55c3b1992bf2df69
dev-montgomery <montgomery.richard.dev@gmail.com>
2024-03-21 12:15:51 -0700

    challenge completed

--- 4kyu/all_balanced_parentheses/solution.js
+++ 4kyu/all_balanced_parentheses/solution.js
@@ -2,3 +2,24 @@
 
 const kata = module.exports = {};
 
+kata.balancedParens = (n) => {
+  const parens = [];
+
+  const createParens = (left, right, current) => {
+    if (left === 0 && right === 0) {
+      parens.push(current);
+      return;
+    };
+
+    if (left > 0) {
+      createParens(left - 1, right, current + '(');
+    };
+
+    if (right > left) {
+      createParens(left, right - 1, current + ')');
+    };
+  };
+
+  createParens(n, n, '');
+  return parens;
+};
--- 4kyu/all_balanced_parentheses/solution.test.js
+++ 4kyu/all_balanced_parentheses/solution.test.js
@@ -2,3 +2,30 @@
 
 const kata = require('./solution.js');
 
+const input1 = 0;
+const output1 = [""];
+
+const input2 = 1;
+const output2 = ["()"];
+
+const input3 = 2;
+const output3 = ["(())","()()"];
+
+const input4 = 3;
+const output4 = ["((()))","(()())","(())()","()(())","()()()"];
+
+test('Should return an array of total "balanced" parentheses based on a given number of parentheses', () => {
+  expect(kata.balancedParens(input1)).toEqual(output1);
+});
+
+test('Should return an array of total "balanced" parentheses based on a given number of parentheses', () => {
+  expect(kata.balancedParens(input2)).toEqual(output2);
+});
+
+test('Should return an array of total "balanced" parentheses based on a given number of parentheses', () => {
+  expect(kata.balancedParens(input3)).toEqual(output3);
+});
+
+test('Should return an array of total "balanced" parentheses based on a given number of parentheses', () => {
+  expect(kata.balancedParens(input4)).toEqual(output4);
+});
